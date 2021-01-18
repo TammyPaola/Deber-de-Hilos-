862d771b1dd9b630a4cca963819564cfd125b407
TammyPaola <tpc.acosta@yavirac.edu.ec>
2021-01-17 21:44:57 -0500

    Deber de Hilos

---
+++ HilosApp/Carrera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+namespace HilosApp
+{
+    class Carrera
+    {
+        public  static void Corredor(object obj)
+        {
+            var nombre = (string)obj;
+            var hiloActual = Thread.CurrentThread;
+            var pasos = 0;
+            var random = new Random();
+            for (int i = 0; i <= 100; i++)
+            {
+                pasos += random.Next(0, 11);
+
+                Console.WriteLine("Jugador {0} dio {1} pasos", hiloActual.ManagedThreadId,pasos);
+                Thread.Sleep(10);
+            }
+            Console.WriteLine("El Jugador {0} llego  a la meta ", hiloActual.ManagedThreadId);
+        }
+    }
+}
---
+++ HilosApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+namespace HilosApp
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("    Bienvenido a mi juego de carreras     ");
+            Console.WriteLine("     Mi nombre es Paola Acosta       ");
+
+            Thread Jugador1 = new Thread(Carrera.Corredor);
+            Thread Jugador2 = new Thread(Carrera.Corredor);
+            Thread Jugador3 = new Thread(Carrera.Corredor);
+            Thread jugador4 = new Thread(Carrera.Corredor);
+
+            Jugador1.Start();
+            Jugador2.Start();
+            Jugador3.Start();
+            jugador4.Start();
+
+            Jugador1.Join();
+            Jugador2.Join();
+            Jugador3.Join();
+            jugador4.Join();
+
+        }
+        static void MetodoPorEjecturar()
+        {
+            var hiloActual = Thread.CurrentThread;
+            hiloActual.CurrentCulture = new System.Globalization.CultureInfo("en-US"); 
+            Console.WriteLine("Hilo actual {0}: ", hiloActual.ManagedThreadId);
+            Console.WriteLine("Mi Cultura es {0} :", hiloActual.CurrentCulture);
+            var random = new Random();
+            for (int i =0; i <= 10; i++)
+            {
+                Console.WriteLine("Hilo {0} indice {1}", hiloActual.ManagedThreadId, i);
+                Thread.Sleep(random.Next(50,75));
+            }
+        }
+    }
+}
